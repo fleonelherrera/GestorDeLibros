754dee4f72d0526c411c5d871564454ebbceafea
Franco <francoherrera77003@gmail.com>
2025-02-15 00:18:47 -0300

    Agrego controllers para acceder a la BD y el modelo Libro

---
+++ Controllers/AccesoDatos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class AccesoDatos
+    {
+        private string cadenaConexion;
+
+        public AccesoDatos()
+        {
+            cadenaConexion = "Server=localhost;Database=GestorLibros;TrustServerCertificate=true;Trusted_Connection=true;MultipleActiveResultSets=true";
+        }
+
+        public DataTable EjecutarConsulta(string nombreSP, SqlParameter[] parametros)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(cadenaConexion))
+                {
+                    using (SqlCommand comando = new SqlCommand(nombreSP, conn))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+
+                        // VALIDO QUE SE PASEN PARAMETROS
+                        if (parametros != null)
+                        {
+                            comando.Parameters.AddRange(parametros);
+                        }
+
+                        SqlDataAdapter da = new SqlDataAdapter(comando);
+
+                        DataTable dt = new DataTable();
+
+                        // PASO EL RESULTADO DEL SELECT A UNA DATATABLE
+                        da.Fill(dt);
+
+                        return dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al querer conectar con la BD" + ex.Message);
+            }
+        }
+
+        public int EjecutarNonQuery(string nombreSP, SqlParameter[] parametros)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(cadenaConexion))
+                {
+                    using (SqlCommand comando = new SqlCommand(nombreSP, conn))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+
+                        if (parametros != null)
+                        {
+                            comando.Parameters.AddRange(parametros);
+                        }
+
+                        conn.Open();
+
+                        return comando.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al querer conectar con la BD" + ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/LibroModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class LibroModel
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Genero { get; set; }
+        public int FechaPublicacion { get; set; }
+        public string Editorial { get; set; }
+        public string Estado { get; set; }
+        public decimal Precio { get; set; }
+        public string UrlImagen { get; set; }
+    }
+}
